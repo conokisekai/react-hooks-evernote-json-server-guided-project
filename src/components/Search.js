b238d1dcae68518cf928ea3ff3d4c810213cf7f7
Conrad Kambi <kambiconrad@gmail.com>
2023-11-10 22:59:55 +0300

    Update Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function Search() {
+function Search({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
   return (
     <div className="filter">
-      <input id="search-bar" type="text" placeholder="Search Notes" />
+      <input
+        id="search-bar"
+        type="text"
+        placeholder="Search Notes"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 }
