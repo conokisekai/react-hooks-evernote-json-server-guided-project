e53e3c09756f8dddd35dd7b7ca29aa501b9d42cb
Conrad Kambi <kambiconrad@gmail.com>
2023-11-10 22:52:09 +0300

    Update NoteContainer component

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,15 +1,51 @@
-import React from "react";
-import Search from "./Search";
-import Sidebar from "./Sidebar";
-import Content from "./Content";
+import React, { useState, useEffect } from 'react';
+import Search from './Search';
+import Sidebar from './Sidebar';
+import Content from './Content';
 
 function NoteContainer() {
+  useEffect(() => {}, []);
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  const handleNoteClick = (note) => {
+    setSelectedNote(note);
+    setEditMode(false);
+  };
+
+  const handleNewNote = () => {};
+
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleSave = (updatedNote) => {
+    setEditMode(false);
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+  };
+
   return (
     <>
-      <Search />
+      <Search onSearch={handleSearch} />
       <div className="container">
-        <Sidebar />
-        <Content />
+        <Sidebar
+          notes={notes}
+          selectedNote={selectedNote}
+          onNoteClick={handleNoteClick}
+          onNewNote={handleNewNote}
+        />
+        <Content
+          selectedNote={selectedNote}
+          editMode={editMode}
+          handleEdit={handleEdit}
+          handleSave={handleSave}
+          handleCancel={handleCancel}
+        />
       </div>
     </>
   );
