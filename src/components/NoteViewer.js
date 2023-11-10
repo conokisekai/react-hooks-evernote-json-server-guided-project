74ef89137c178c0a3253a66ae49164682de5fde2
Conrad Kambi <kambiconrad@gmail.com>
2023-11-11 01:07:27 +0300

    Implemented Email and Share functionality

--- src/components/NoteViewer.js
+++ src/components/NoteViewer.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-function NoteViewer({ note, onEdit, onShare, onEmail }) {
+const NoteViewer = ({ note, onEdit, onCancel, onShare, onEmail }) => {
+  const handleShare = () => {
+    console.log('Sharing note:', note);
+  };
+
+  const handleEmail = () => {
+    console.log('Emailing note:', note);
+
+    const emailSubject = encodeURIComponent(`Shared Note: ${note.title}`);
+    const emailBody = encodeURIComponent(note.body);
+    const mailtoLink = `mailto:?subject=${emailSubject}&body=${emailBody}`;
+    window.open(mailtoLink);
+  };
+
   return (
     <>
       <h2>{note.title}</h2>
       <p>{note.body}</p>
       <button onClick={onEdit}>Edit</button>
+      <button onClick={onCancel}> Cancel</button>
       <button onClick={onShare}>Share</button>
       <button onClick={onEmail}>Email</button>
     </>
   );
-}
+};
 
 export default NoteViewer;
