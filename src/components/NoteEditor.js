69b8e8881fdd8d196e512d19607e894ca2873c4a
Conrad Kambi <kambiconrad@gmail.com>
2023-11-10 22:54:42 +0300

    Update NoteEditor component

--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function NoteEditor() {
+function NoteEditor({ note, onSave, onCancel }) {
+  const [editedNote, setEditedNote] = useState({ ...note });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedNote((prevNote) => ({ ...prevNote, [name]: value }));
+  };
+
+  const handleSaveClick = () => {
+    onSave(editedNote);
+  };
+
+  const handleCancelClick = () => {
+    onCancel();
+  };
+
   return (
     <form className="note-editor">
-      <input type="text" name="title" />
-      <textarea name="body" />
+      <input
+        type="text"
+        name="title"
+        value={editedNote.title}
+        onChange={handleChange}
+      />
+      <textarea name="body" value={editedNote.body} onChange={handleChange} />
       <div className="button-row">
-        <input className="button" type="submit" value="Save" />
-        <button type="button">Cancel</button>
+        <input
+          className="button"
+          type="button"
+          value="Save"
+          onClick={handleSaveClick}
+        />
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
       </div>
     </form>
   );
