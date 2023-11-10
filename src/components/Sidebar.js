5a586638c686f496b9e9533b1ed65ba4f4afd00c
Conrad Kambi <kambiconrad@gmail.com>
2023-11-10 23:01:15 +0300

    Update Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
-import React from "react";
-import NoteList from "./NoteList";
+import React from 'react';
+import NoteList from './NoteList';
 
-function Sidebar() {
+function Sidebar({ notes, selectedNote, onNoteClick, onNewNote }) {
   return (
     <div className="master-detail-element sidebar">
-      <NoteList />
-      <button>New</button>
+      <NoteList notes={notes} onNoteClick={onNoteClick} />
+      <button onClick={onNewNote}>New</button>
     </div>
   );
 }
